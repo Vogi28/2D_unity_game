95e2bbd2f1e0a0f4b0b32ee1da5231b0475c70fd
Vogi <vogi.nguyen@live.fr>
2020-02-28 14:34:59 +0100

    slime animation
    movment et idle + script

--- Assets/Ressources/Scripts/SlimeBehaviour.cs
+++ Assets/Ressources/Scripts/SlimeBehaviour.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SlimeBehaviour : MonoBehaviour
 {
     public float mvSpeed = 5f;
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +17,11 @@ public class SlimeBehaviour : MonoBehaviour
     {
         Vector3 mv = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += mv * Time.deltaTime * mvSpeed;
+
+        if (Input.GetAxis("Horizontal") == -1 || Input.GetAxis("Horizontal") == 1)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        
     }
 }
