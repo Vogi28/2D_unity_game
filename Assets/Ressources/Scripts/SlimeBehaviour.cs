55a75c485d756733410834902ed292def4bcf074
Vogi <vogi.nguyen@live.fr>
2020-02-28 15:37:11 +0100

    function jump
    jump de base

--- Assets/Ressources/Scripts/SlimeBehaviour.cs
+++ Assets/Ressources/Scripts/SlimeBehaviour.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class SlimeBehaviour : MonoBehaviour
 {
-    public float mvSpeed = 5f;
+    public Rigidbody2D rb;
+    public float speed = 5f;
+    private bool isGrounded = false;
     public Animator animator;
     // Start is called before the first frame update
     void Start()
@@ -16,12 +18,41 @@ public class SlimeBehaviour : MonoBehaviour
     void Update()
     {
         Vector3 mv = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += mv * Time.deltaTime * mvSpeed;
+        transform.position += mv * Time.deltaTime * speed;
+        //horizontalMove = Input.GetAxis("Horizontal") * mvSpeed;
 
-        if (Input.GetAxis("Horizontal") == -1 || Input.GetAxis("Horizontal") == 1)
+        if (Input.GetAxis("Horizontal") != 0)
         {
             animator.SetBool("isMoving", true);
         }
-        
+
+        if (Input.GetKeyDown("space") && isGrounded)
+        {
+            jump();
+        }
     }
+
+    void jump()
+    {
+        //Vector3 jp = new Vector3(0f, speed, 0f);
+        //transform.position += jp * Time.deltaTime * speed;
+        rb.velocity += new Vector2(0, speed);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
 }
